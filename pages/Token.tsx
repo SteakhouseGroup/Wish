75d0fbf8d00e6d0012b3b530e0a64ce552d6cab8
SteakhouseGroup <128934133+SteakhouseGroup@users.noreply.github.com>
2023-07-15 18:34:38 +0100

    Update Token.tsx

--- pages/Token.tsx
+++ pages/Token.tsx
@@ -12,14 +12,27 @@ import {
   Button,
 } from "@chakra-ui/react";
 import { NextPage } from "next";
+import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
 
 const Token: NextPage = () => {
+  const [showItems, setShowItems] = useState(false);
+  const [isPressed, setIsPressed] = useState(false);
+
+  const handleBoxClick = () => {
+    setIsPressed(true);
+  };
+
+  useEffect(() => {
+    setShowItems(true);
+  }, []);
+
   return (
     <Box
       display="flex"
       alignItems="center"
       justifyContent="center"
-      height="100vh" // Ensure the parent Box takes the full viewport height
+      height="85vh" // Ensure the parent Box takes the full viewport height
     >
       <SimpleGrid
         columns={{ base: 1, md: 2, lg: 2 }}
@@ -27,14 +40,86 @@ const Token: NextPage = () => {
         margin="auto"
         spacing={4}
       >
-        <Box
-          margin="auto"
-          alignItems="center"
-          justifyContent="center"
-          textAlign={"center"}
+        <motion.a
+          href="/mint"
+          rel="noopener noreferrer"
+          initial={{ y: 100, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.5 }}
+          onClick={handleBoxClick}
         >
-          <Image h={250} w={250} src="/images/Logo.jpeg" />
-        </Box>
+          <motion.div
+            whileHover={{ translateY: -4, boxShadow: "2xl" }}
+            whileTap={{ scale: 0.95 }}
+            style={isPressed ? { transform: "scale(0.95)" } : {}}
+          >
+            <Box bg="white" borderRadius="md" boxShadow="xl" p={4}>
+              <VStack spacing={2} textAlign="center">
+                <Image
+                  src="https://cdn.discordapp.com/attachments/1126876974179172424/1129817552692981832/Skin_Noise.png"
+                  alt="Placeholder preview of starter"
+                  width={200}
+                  height={200}
+                  borderRadius={"md"}
+                  boxShadow="xl"
+                />
+              </VStack>
+            </Box>
+          </motion.div>
+        </motion.a>
+        <motion.a
+          href="/mint"
+          rel="noopener noreferrer"
+          initial={{ y: 100, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.5 }}
+          onClick={handleBoxClick}
+        >
+          <motion.div
+            whileHover={{ translateY: -4, boxShadow: "2xl" }}
+            whileTap={{ scale: 0.95 }}
+            style={isPressed ? { transform: "scale(0.95)" } : {}}
+          >
+            <Box bg="white" borderRadius="md" boxShadow="xl" p={4}>
+              <VStack spacing={2} textAlign="center">
+                <Image
+                  src="https://cdn.discordapp.com/attachments/1126876974179172424/1129817554332942437/IMG_4034.png"
+                  alt="Placeholder preview of starter"
+                  width={200}
+                  height={200}
+                  borderRadius={"md"}
+                  boxShadow="xl"
+                />
+              </VStack>
+            </Box>
+          </motion.div>
+        </motion.a>
+        <motion.a
+          href="/mint"
+          rel="noopener noreferrer"
+          initial={{ y: 100, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.5 }}
+          onClick={handleBoxClick}
+        >
+          <motion.div
+            whileHover={{ translateY: -4, boxShadow: "2xl" }}
+            whileTap={{ scale: 0.95 }}
+            style={isPressed ? { transform: "scale(0.95)" } : {}}
+          >
+            <Box bg="white" borderRadius="md" boxShadow="xl" p={4}>
+              <VStack spacing={2} textAlign="center">
+                <Image
+                  width={200}
+                  height={200}
+                  borderRadius={"md"}
+                  boxShadow="xl"
+                  src="/images/Logo.jpeg"
+                />
+              </VStack>
+            </Box>
+          </motion.div>
+        </motion.a>
         <VStack>
           <Box
             margin="auto"
@@ -62,6 +147,61 @@ const Token: NextPage = () => {
             style={{ width: "80%", height: "80vh" }}
           ></iframe>
         </VStack>
+
+        <motion.a
+          href="/mint"
+          rel="noopener noreferrer"
+          initial={{ y: 100, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.5 }}
+          onClick={handleBoxClick}
+        >
+          <motion.div
+            whileHover={{ translateY: -4, boxShadow: "2xl" }}
+            whileTap={{ scale: 0.95 }}
+            style={isPressed ? { transform: "scale(0.95)" } : {}}
+          >
+            <Box bg="white" borderRadius="md" boxShadow="xl" p={4}>
+              <VStack spacing={2} textAlign="center">
+                <Image
+                  src="https://cdn.discordapp.com/attachments/1126876974179172424/1129817555045986466/Skin_Caramel.png"
+                  alt="Placeholder preview of starter"
+                  width={200}
+                  height={200}
+                  borderRadius={"md"}
+                  boxShadow="xl"
+                />
+              </VStack>
+            </Box>
+          </motion.div>
+        </motion.a>
+        <motion.a
+          href="/mint"
+          rel="noopener noreferrer"
+          initial={{ y: 100, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.5 }}
+          onClick={handleBoxClick}
+        >
+          <motion.div
+            whileHover={{ translateY: -4, boxShadow: "2xl" }}
+            whileTap={{ scale: 0.95 }}
+            style={isPressed ? { transform: "scale(0.95)" } : {}}
+          >
+            <Box bg="white" borderRadius="md" boxShadow="xl" p={4}>
+              <VStack spacing={2} textAlign="center">
+                <Image
+                  src="https://cdn.discordapp.com/attachments/1126876974179172424/1129817559919755375/Skin_White_Peach.png"
+                  alt="Placeholder preview of starter"
+                  width={200}
+                  height={200}
+                  borderRadius={"md"}
+                  boxShadow="xl"
+                />
+              </VStack>
+            </Box>
+          </motion.div>
+        </motion.a>
       </SimpleGrid>
     </Box>
   );
